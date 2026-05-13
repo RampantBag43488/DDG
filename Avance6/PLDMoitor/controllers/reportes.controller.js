2b21c61884071d2e2ad1a27147054e2caa035264
leofu <a01614731@tec.com>
2026-05-13 15:18:37 -0600

    Add of PLDMonitor proyect

---
+++ Avance6/PLDMoitor/controllers/reportes.controller.js
@@ -0,0 +1,16 @@
+const model = require('../models/reportes.model.js');
+
+module.exports.index = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = 5;
+        const reportes = await model.fetchAll(page, pageSize);
+        const total = await model.countReportes();
+        const totalPages = Math.ceil(total / pageSize);
+        res.render('oc/reportes/Reportes', { reportes, total, page, pageSize, totalPages });
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send('Error al cargar reportes');
+    }
+};
