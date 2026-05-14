526ee3e56abc77faad510e546cfa55e1bd44d0f3
leofu <a01614731@tec.com>
2026-05-14 17:07:54 -0600

    New view for create a new expedient and views for employee

--- Avance6/PLDMoitor/controllers/expediente.controller.js
+++ Avance6/PLDMoitor/controllers/expediente.controller.js
@@ -15,6 +15,29 @@ module.exports.index = async (req, res) => {
     }
 };
 
+module.exports.nuevo = (req, res) => {
+    res.render('oc/expediente/NuevoExpediente');
+};
+
+module.exports.nuevoEmpleado = (req, res) => {
+    res.render('empleado/expedientes/NuevoExpediente');
+};
+
+module.exports.indexEmpleado = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = 5;
+        const expedientes = await model.fetchAll(page, pageSize);
+        const total = await model.countExpedientes();
+        const totalPages = Math.ceil(total / pageSize);
+        res.render('empleado/expedientes/Expedientes', { expedientes, total, page, pageSize, totalPages });
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send('Error al cargar expedientes');
+    }
+};
+
 module.exports.getExpedientes = async (req, res) => {
     const expedientes = await model.getExpedientes();
     res.status(200).json({ total: expedientes});
--- Avance6/PLDMoitor/controllers/reportes.controller.js
+++ Avance6/PLDMoitor/controllers/reportes.controller.js
@@ -14,3 +14,18 @@ module.exports.index = async (req, res) => {
         res.status(500).send('Error al cargar reportes');
     }
 };
+
+module.exports.indexEmpleado = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = 5;
+        const reportes = await model.fetchAll(page, pageSize);
+        const total = await model.countReportes();
+        const totalPages = Math.ceil(total / pageSize);
+        res.render('empleado/reportes/Reportes', { reportes, total, page, pageSize, totalPages });
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).send('Error al cargar reportes');
+    }
+};
