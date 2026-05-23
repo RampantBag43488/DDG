a759b423333ce0c45cc6825e218aeed7c5ad62e0
Juan Andres Elizalde Herrera <A01708389@tec.mx>
2026-05-22 22:50:19 -0600

    Correcion operacion

--- Avance7/PLDMoitor/controllers/operaciones.controller.js
+++ Avance7/PLDMoitor/controllers/operaciones.controller.js
@@ -5,25 +5,36 @@ module.exports.index = async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
         const pageSize = 10;
-        const operaciones = await model.fetchAll(page, pageSize);
-        const total = await model.countOperaciones();
-        const totalPages = Math.ceil(total /pageSize);
-        res.render('oc/operaciones/Operaciones',{operaciones, total, page, pageSize, totalPages});
+
+        //filtros (SIEMPRE van aquí)
+        const search = req.query.search || '';
+        const tipo = req.query.tipo || '';
+        const fecha = req.query.fecha || '';
+
+        //consulta con o sin filtro
+        const operaciones = await model.fetchAll(page, pageSize, search, tipo, fecha);
+        const total = await model.countOperacionesFiltered(search, tipo, fecha);
+        const totalPages = Math.ceil(total / pageSize);
+
+        //bitácora
         await registrarBitacora({
             id_usuario: req.session.id_usuario,
             accion: 'Visualizó Operaciones',
             descripcion: `Visualización de operaciones por usuario ${req.session.nombre}`
         });
 
+        //render
+        res.render('oc/operaciones/Operaciones', {
+            operaciones,
+            total,
+            page,
+            pageSize,
+            totalPages,
+            search,
+            tipo,
+            fecha
+        });
 
-    }catch(e){
-        const search = req.query.search || '';
-        const tipo = req.query.tipo || '';
-        const fecha = req.query.fecha || '';
-        const operaciones = await model.fetchAll(page, pageSize, search, tipo, fecha);
-        const total = await model.countOperacionesFiltered(search, tipo, fecha);
-        const totalPages = Math.ceil(total / pageSize);
-        res.render('oc/operaciones/Operaciones', { operaciones, total, page, pageSize, totalPages, search, tipo, fecha });
     } catch (e) {
         console.log(e);
         res.status(500).send('Error al cargar operaciones');
